0ae455e2ce5bfcea89287e2bf8de58c435004f1f
Oreyato <oreyato.pro@gmail.com>
2022-09-03 16:38:15 +0200

    Persistence between session

---
+++ Assets/Scripts/DataHolder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class DataHolder : MonoBehaviour
+{
+    public static DataHolder Instance;
+
+    public string PlayerName;
+    public int BestScore;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadPlayerDatas();
+    }
+    
+    // Data persistence between sessions ==============================================
+    [System.Serializable]
+    class SaveData
+    {
+        public int BestScore;
+        public string PlayerName;
+    }
+    
+    public void SavePlayerDatas()
+    {
+        SaveData data = new SaveData
+        {
+            BestScore = BestScore,
+            PlayerName = PlayerName
+        };
+
+        // Store data in json format
+        string json = JsonUtility.ToJson(data);
+        
+        // Send it to a file
+        File.WriteAllText(Application.persistentDataPath + "/playerDatas.json", json);
+    }
+    
+    public void LoadPlayerDatas()
+    {
+        string path = Application.persistentDataPath + "/playerDatas.json";
+        
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            PlayerName = data.PlayerName;
+            BestScore = data.BestScore;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SavePlayerName()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
